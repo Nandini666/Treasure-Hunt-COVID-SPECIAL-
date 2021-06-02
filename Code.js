d38c8d97f0e85bf5421f8af4f8171164dfd917ae
Nandini666 <75084677+Nandini666@users.noreply.github.com>
2021-06-02 19:52:27 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "CHINA";
+const accessCode2 = "ISOLATE";
+const accessCode3 = "MASK";
+
+function clues() {
+    
+    fill("black")
+    textSize(15)
+    text("A N I H C", 100,50)
+    fill("black")
+    text("Hint: It is in Asia and above India !!", 100,70)
+
+    fill("black")
+    textSize(15)
+    text("E  T A L O S I", 580,150)
+    fill("black")
+    text("Hint:Should be in a place alone!!", 580,170)
+
+    fill("black")
+    textSize(15)
+    text("K S A M", 100,300)
+    fill("black")
+    text("Hint: Should be worn on the face !!", 100,320) 
+// add code for displaying rest of the names and the hints.
+    
+
+}
